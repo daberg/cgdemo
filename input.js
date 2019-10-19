9d688b06b85c54d55f927c565b89bf28521d318e
daberg <daberg@openmailbox.org>
2019-10-19 17:24:11 +0200

    Add user input module

---
+++ input.js
@@ -0,0 +1,60 @@
+var input = (function () {
+    var pub = {};
+
+    pub.fwd = false;
+    pub.bwd = false;
+    pub.uwd = false;
+    pub.dwd = false;
+    pub.lwd = false;
+    pub.rwd = false;
+
+    var handlers = {
+        // A key
+        65: function(state) {
+            pub.lwd = state;
+        },
+        // D key
+        68: function(state) {
+            pub.rwd = state;
+        },
+        // S key
+        83: function(state) {
+            pub.bwd = state;
+        },
+        // W key
+        87: function(state) {
+            pub.fwd = state;
+        },
+        // Q key
+        81: function(state) {
+            pub.dwd = state;
+        },
+        // E key
+        69: function(state) {
+            pub.uwd = state;
+            console.log(state);
+        }
+    };
+
+    window.addEventListener(
+        "keydown",
+        function(e) {
+            if (e.keyCode in handlers) {
+                handlers[e.keyCode](true);
+            }
+        },
+        false
+    );
+
+    window.addEventListener(
+        "keyup",
+        function(e) {
+            if (e.keyCode in handlers) {
+                handlers[e.keyCode](false);
+            }
+        },
+        false
+    );
+
+    return pub;
+})();
