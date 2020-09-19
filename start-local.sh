7e334c77ca83a35d2f43c36c5f641c4b31a596ef
daberg <davide.beramaschi@protonmail.com>
2020-09-19 22:18:57 +0100

    Improve running scripts

--- start-local.sh
+++ start-local.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-webfsd -p 8080 -f index.html -n localhost
+if [ -f /tmp/cgdemopid ]; then
+    echo "An instance is already running ($(cat /tmp/cgdemopid))"
+    exit 1
+fi
+
+webfsd -p 8080 -f index.html -n localhost -k /tmp/cgdemopid && echo "Started instance ($(cat /tmp/cgdemopid))"
--- stop-local.sh
+++ stop-local.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-killall -q webfsd
+if [ ! -f /tmp/cgdemopid ]; then
+    echo "No instance running"
+    exit 1
+fi
+
+pid=$(cat /tmp/cgdemopid)
+
+kill $pid && echo "Instance killed ($pid)"
