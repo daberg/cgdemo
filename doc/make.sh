4c00878ed447302913b054fc00bfeee6d65ad09e
daberg <davide.bergamaschi@protonmail.com>
2021-08-23 22:02:29 +0100

    Change presentation as handout

--- doc/make.sh
+++ doc/make.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-pandoc -t beamer -H header.tex -o presentation.pdf -i presentation.md
+if [ -n "$1" ] && [ "$1" = "--handout" ]
+then
+    pandoc -t beamer -H header.tex -o presentation.pdf -i presentation.md --defaults options
+else
+    pandoc -t beamer -H header.tex -o presentation.pdf -i presentation.md
+fi
